f48d3211418b72b926d41e77bdb46f02cad8c6ab
Prince vasoya <vasoyaprince14@gmail.com>
2024-04-02 10:57:50 +0530

    Initial Commit

---
+++ Favorite Athletes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Favorite Athletes
+//
+//  Created by Prince Vasoya on 02/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
